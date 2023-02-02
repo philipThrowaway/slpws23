6378fddfa12e11235bdf3e1259263e8da9d6ca43
philipThrowaway <95085166+philipThrowaway@users.noreply.github.com>
2023-02-02 11:50:09 +0100

    autentisering kontroll, dessutom "error" hantering

--- app.rb
+++ app.rb
@@ -3,46 +3,112 @@ require 'sinatra/reloader'
 require 'slim'
 require 'bcrypt'
 
-require_relative 'lib/helpers.rb'
+require_relative './model.rb'
 
 enable :sessions
 DB_PATH ||= 'db/database.db'
 
-def is_authenticated?()
-    id = session[:id].to_i
-    if id then true else false end
-end
-
-def auth_gate()
-    if not is_authenticated?()
-        redirect('/')
-    end 
-end
-
-get('/') do
-    if is_authenticated?() # check if user hasn't logged in
-        redirect('/login')
-    else
-        redirect('/rooms')
+before do 
+    p request.path_info
+    if (session[:id] == nil) && !['/', '/error', '/login', '/register'].include?(request.path_info)
+        session[:error] = "You need to be logged in"
+        redirect('/error')
     end
 end
 
+after do 
+    if (request.path_info != '/error')
+        # session[:prev_action] = request.path_info
+    end
+end
+
+get('/error') do 
+    @error_message = session[:error]
+    @prev_action = session[:prev_action]
+    p @prev_action
+    slim(:error)
+end
+
+get('/') do
+    redirect('/login')
+end
+
 get('/login') do
+    session[:prev_action] = request.path_info
     slim(:login)
 end
 
-post('/login') do 
-    "success"
-end
+post('/login') do
+    db = Database::connect(DB_PATH)
+    username = params[:username]
+    password = params[:password]
+  
+    result = db.execute("SELECT * FROM user WHERE username=?", username).first
+    if result
+      pw_digest = result["pw_digest"]
+      id = result["id"]
+  
+      if BCrypt::Password.new(pw_digest) == password
+        session[:id] = id
+        redirect('/rooms')
+      else  
+        # wrong password
+        session[:error] = "Wrong password"
+        redirect('/error')
+      end
+    else  
+      # user doesn't exist
+      session[:error] = "User doesn't exist"
+      redirect('/error')
+    end
+  end
 
 get('/register') do
+    session[:prev_action] = request.path_info
     slim(:"users/new")
 end
 
-post('/users/') do
+get('/users/') do 
+    session[:prev_action] = request.path_info
+    p "inside users"
+    slim(:"users/index")
+end
+
+post('/users') do
     db = Database::connect(DB_PATH)
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+
+    result = db.execute("SELECT id FROM user WHERE username=?", username)
+    if result.empty?
+        if (password == password_confirm)
+          pw_digest = BCrypt::Password.create(password)
+          
+          db.execute("INSERT INTO user(username, pw_digest) VALUES (?, ?)", username, pw_digest)
+          redirect('/')
+        else 
+            # passwords don't match
+            session[:error] = "Password doesn't match"
+            redirect('/error')
+        end
+    else  
+        # username doesn't exist
+        session[:error] = "User doesn't exist"
+        redirect('/error')
+    end
 end
 
 get('/rooms') do
+    session[:prev_action] = request.path_info
     db = Database::connect(DB_PATH)
+    slim(:"rooms/index")
 end
+
+
+
+# ge varje room en kategori 
+# för att få ytterliggare en relationstabell
+
+# låt också det finnas viss användar
+# behövrighet 
--- model.rb
+++ model.rb
@@ -1,9 +1,23 @@
 require 'sqlite3'
+
 module Database
     def self.connect(path)
         p "connecting to #{@path}"
         db = SQLite3::Database.new(path)
         db.results_as_hash = true
         return db
+    end
+end
+
+module Auth
+    def self.init()
+    end
+
+    def self.is_authenticated?()
+        if session[:id] then true else false end
+    end
+
+    def self.redirect(url)
+        
     end
 end
