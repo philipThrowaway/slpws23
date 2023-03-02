3f96d40952f1bd5bd03758dba0e0d100c1a03553
philipThrowaway <95085166+philipThrowaway@users.noreply.github.com>
2023-03-02 11:50:12 +0100

    rum funktionalitet (taggar, medlemmar) admin verktyg

--- app.rb
+++ app.rb
@@ -122,7 +122,7 @@ post('/users') do
         redirect('/error')
     end
 
-    if username.scan(/\s/) || password.scan(/\s/)
+    if username.scan(/\s/).length > 0 || password.scan(/\s/).length > 0
         session[:error] = "One of the parameters had a whitespace character in it."
         redirect('/error')
     end
@@ -148,7 +148,16 @@ end
 
 get('/rooms/') do
     session[:prev_action] = request.path_info
+    id = session[:id]
     db = Database::connect(DB_PATH)
+
+    @rooms = db.execute("
+        SELECT r.id AS room_id, r.name AS room_name, u.username AS owner_username, COUNT(rur.user_id) AS member_count
+        FROM room AS r
+        JOIN user AS u ON u.id = r.owner_id
+        LEFT JOIN room_user_relation AS rur ON rur.room_id = r.id
+        GROUP BY r.id")
+
     slim(:"rooms/index")
 end
 
@@ -161,12 +170,63 @@ get('/rooms/new') do
 end
 
 post('/rooms') do 
+    db = Database::connect(DB_PATH)
+    name = params[:name]
+    password = params[:password]
+    tag_ids = params[:tags]
+    owner_id = session[:id]
+
+    if name.length > 16
+        p "name length"
+        session[:error] = "The room name must be at most 16 characters long."
+        redirect('/error')
+    end
+
+    if name.scan(/\s/).length > 0 || password.scan(/\s/).length > 0
+        p "whitespace"
+        p name
+        p password
+        session[:error] = "One of the parameters had a whitespace character in it."
+        redirect('/error')
+    end
+
+    pw_digest = BCrypt::Password.create(password)
+
+    existing_room = db.execute("SELECT id FROM room WHERE name = ?", name).first
+    if existing_room
+        # room already exists
+        p "room exists"
+        session[:error] = "A room with the name '#{name}' already exists."
+        redirect('/error')
+    end
+
+    db.execute("INSERT INTO room(name, pw_digest, owner_id) VALUES (?, ?, ?)", name, pw_digest, owner_id)
+    room_id = db.last_insert_row_id
+    tag_ids.each do |tag_id|
+        tag = tag_id.to_i
+        db.execute("INSERT INTO room_tags_relation(room_id, tag_id) VALUES (?, ?)", room_id, tag)
+    end      
+
+    # maybe add a check?
+    db.execute("INSERT INTO room_user_relation(room_id, user_id) VALUES (?, ?)", room_id, owner_id)
+    
     redirect('/rooms/')
 end
+
 
 get('/admin/') do 
     session[:prev_action] = request.path_info
     slim(:"admin/index")
+end
+
+post('/admin/nuke') do
+    db = Database::connect(DB_PATH)
+    db.execute("DELETE FROM room")
+    db.execute("DELETE FROM room_user_relation")
+    db.execute("DELETE FROM room_tags_relation")
+
+  # redirect to admin page
+    redirect('/admin/')
 end
 
 get('/tags/new') do
